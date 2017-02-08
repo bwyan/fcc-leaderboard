7b7d03dd1141c0b93d1133925519ef655a8677b0
Bryan Wegman <xxbw@icloud.com>
2017-02-07 20:48:04 -0800

    Improve styling and layout.

--- src/Leaderboard.js
+++ src/Leaderboard.js
@@ -5,12 +5,19 @@ class Leaderboard extends React.Component {
 		return(
 			<div>
 				<table>
+					<colgroup>
+						<col className="col-rank" />
+						<col className="col-avatar" />
+						<col className="col-username" />
+						<col className="col-points" />
+						<col className="col-points" />
+					</colgroup>
 					<tbody>
 						{this.props.campers.map(camper =>
 							<tr key={camper.username}>
 								<td className="cell-rank">{this.props.campers.indexOf(camper) + 1}</td>
-								<td className="cell-avatar"><img src={camper.img} alt="Avatar" className="avatar" /></td>
-								<td className="cell-username">{camper.username}</td>
+								<td className="cell-avatar"><a href={`https://www.freecodecamp.com/${camper.username}`} target="_blank"><img src={camper.img} alt="Avatar" className="avatar" /></a></td>
+								<td className="cell-username"><a href={`https://www.freecodecamp.com/${camper.username}`} target="_blank">{camper.username}</a></td>
 								<td className={(this.props.listType === "recent") ? "cell-points bold" : "cell-points"}>{camper.recent}</td>
 								<td className={(this.props.listType === "alltime") ? "cell-points bold" : "cell-points"}>{camper.alltime}</td>
 							</tr>
